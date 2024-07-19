e8a72127631c262cf85ace46d0055b7bf662daef
xepiqq <jaydencrowther@gmail.com>
2024-07-18 22:32:30 -0600

    maybe fixed signin bug

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { getCurrentUser } from "@/lib/session";
-import Bigboi from './bigboi'
+import Bigboi from './bigboi';
 
 export default async function Login() {
-
   const user = await getCurrentUser();
 
   return (
     <div className="h-screen w-screen">
-      <Bigboi user={user}/>
+      <Bigboi user={user} />
     </div>
   );
 }
