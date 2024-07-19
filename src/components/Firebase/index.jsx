37c37996ad6b1923267b5d7267a0aa6c9ba89a2d
xepiqq <jaydencrowther@gmail.com>
2024-07-19 00:16:17 -0600

    re-expose these bad bois

--- src/components/Firebase/index.jsx
+++ src/components/Firebase/index.jsx
@@ -1,25 +1,3 @@
-// "use client";
-
-// import firebase from "firebase/app";
-// import 'firebase/auth';
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-
-// const app = initializeApp({
-//   apiKey: "AIzaSyCnh7tVOkW4UsvrqTEDtJPVRlryIibSb1s",
-//   authDomain: "harmonyhive-b4705.firebaseapp.com",
-//   projectId: "harmonyhive-b4705",
-//   storageBucket: "harmonyhive-b4705.appspot.com",
-//   messagingSenderId: "41274838584",
-//   appId: "1:41274838584:web:cdd0eb6e06deb0f0af60cd",
-//   measurementId: "G-E2XH6TESCR",
-// });
-
-// export const firestore = getFirestore(app);
-// export const storage = getStorage(app);
-
 "use client";
 
 import firebase from "firebase/app";
@@ -30,15 +8,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const app = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCnh7tVOkW4UsvrqTEDtJPVRlryIibSb1s",
+  authDomain: "harmonyhive-b4705.firebaseapp.com",
+  projectId: "harmonyhive-b4705",
+  storageBucket: "harmonyhive-b4705.appspot.com",
+  messagingSenderId: "41274838584",
+  appId: "1:41274838584:web:cdd0eb6e06deb0f0af60cd",
+  measurementId: "G-E2XH6TESCR",
 });
 
 export const firestore = getFirestore(app);
 export const storage = getStorage(app);
-
-export default firebase;
